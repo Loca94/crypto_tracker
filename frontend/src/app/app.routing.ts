06be3bf3728c3e734cebf7772fe4185f55737316
Davide Locatelli <davidelocatelli@MacBook-Pro-di-Davide.local>
2022-01-26 21:43:25 +0100

    forgot to add guard to detail coin page

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -26,7 +26,7 @@ const routes: Routes = [
   {
     path: 'my-crypto/coin-detail',
     component: CoinDetailComponent,
-    // canActivate: [AuthGuard]
+    canActivate: [AuthGuard]
   },
   {
     path: 'login',
